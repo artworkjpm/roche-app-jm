3e2b12054827700ef335e754e68b1ee20e9b4163
John <johnnymoran44@hotmail.com>
2019-08-14 16:57:54 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="container">
       <h4 className="center">About</h4>
-      <p>This is an example of a HIGH ORDER FUNCTION, just to change the color of About page text, hit refresh to change color</p>
+      <p>This is an example of a HIGH ORDER FUNCTION, hit refresh to change the color of the text and see the super powers of a HOC!</p>
     </div>
   );
 };
