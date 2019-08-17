b229c011c9ec4a41aa29cf2470ff5ebbc00a98cb
John Moran <johnnymoran44@hotmail.com>
2019-08-17 17:53:00 +0200

    Update graph.js

--- src/components/medico/graph.js
+++ src/components/medico/graph.js
@@ -1,29 +1,75 @@
-import React, { Component } from "react";
-import { LineChart, Line } from "recharts";
+import React, { PureComponent } from "react";
+import { ResponsiveContainer, ComposedChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 
-class Graph extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const data = [
+  {
+    name: "00:00",
+    uv: 590,
+    pv: 1800,
+    amt: [65, 103],
+    cnt: 1000
+  },
+  {
+    name: "Page B",
+    uv: 868,
+    pv: 967,
+    amt: [65, 103],
+    cnt: 1000
+  },
+  {
+    name: "Page C",
+    uv: 1397,
+    pv: 1098,
+    amt: [65, 103],
+    cnt: 1000
+  },
+  {
+    name: "Page D",
+    uv: 1480,
+    pv: 1200,
+    amt: [65, 103],
+    cnt: 1000
+  },
+  {
+    name: "Page E",
+    uv: 1820,
+    pv: 1108,
+    amt: [65, 103],
+    cnt: 1000
+  },
+  {
+    name: "Page F",
+    uv: 1400,
+    pv: 680,
+    amt: [65, 103],
+    cnt: 1000
   }
+];
+
+export default class Graph extends PureComponent {
+  static jsfiddleUrl = "https://jsfiddle.net/alidingling/9xopwa9v/";
+
   render() {
-    const data = [
-      { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-      { name: "Page A", uv: 100, pv: 2400, amt: 2400 },
-      { name: "Page A", uv: 1000, pv: 2400, amt: 2400 },
-      { name: "Page A", uv: 2400, pv: 2400, amt: 2400 },
-      { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-      { name: "Page A", uv: 400, pv: 2400, amt: 2400 }
-    ];
-
-    const renderLineChart = (
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      </LineChart>
+    return (
+      <ResponsiveContainer width="100%" height={400}>
+        <ComposedChart
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            left: 0,
+            bottom: 0
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+        </ComposedChart>
+      </ResponsiveContainer>
     );
-
-    return <div>{renderLineChart}</div>;
   }
 }
-
-export default Graph;
